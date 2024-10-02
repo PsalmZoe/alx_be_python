99fa6cf4516b6083af752fa5aaa2e82dcb8025e4
David Oche Agbidi <psalmzoe007@gmail.com>
2024-10-02 10:53:31 +0100

    New Python File

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+sum_result = number1 + number2
+difference_result = number2 - number1
+product_result = number1 * number2
+
+print(f"Sum of {number1} and {number2} is {sum_result}")
+print(f"difference of {number2} and {number1} is {difference_result}")
+print(f"product of {number1} and {number2} is {product_result}")
